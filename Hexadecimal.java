81e36bb1ba5daea1df494df636b5d198ddd1a6e1
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-17 11:12:40 +0200

    [change] Simplification of class' use

--- Hexadecimal.java
+++ Hexadecimal.java
@@ -1,25 +1,5 @@
 public class Hexadecimal {
-    public static void main(String[] args) {
-        
-    }
-
-    public static String toHexadecimal(int[] asciiArray) {
-        return "";
-    }
-
-    public static String toOctal (int[] asciiArray) {
-        return "";
-    }
-
-    public static String toBinary (int[] asciiArray) {
-        return "";
-    }
-
-    public static String toDecimal (int[] asciiArray) {
-        return "";
-    }
-
-    public static String toText (int[] asciiArray) {
-        return "";
+    public static int[] toAscii (String input) {
+        return new int[0];
     }
 }
--- Text.java
+++ Text.java
@@ -1,25 +1,5 @@
 public class Text {
-    public static void main(String[] args) {
-        
-    }
-
-    public static String toText (int[] asciiArray) {
-        return "";
-    }
-
-    public static String toHexadecimal(int[] asciiArray) {
-        return "";
-    }
-
-    public static String toOctal (int[] asciiArray) {
-        return "";
-    }
-
-    public static String toBinary (int[] asciiArray) {
-        return "";
-    }
-
-    public static String toDecimal (int[] asciiArray) {
-        return "";
+    public static int[] toAscii (String input) {
+        return new int[0];
     }
 }
