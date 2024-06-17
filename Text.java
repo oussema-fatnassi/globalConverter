8dbe93e21084906c26a55ba5a3b25acb51c5b015
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-06-17 13:47:11 +0200

    [change] Conversion from text to ASCII

--- Text.java
+++ Text.java
@@ -1,10 +1,19 @@
 public class Text {
-    public static int[] toAscii (String input) {
+    public static int[] toAscii(String input) {
         int[] asciiArray = new int[input.length()];
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             asciiArray[i] = (int) c;
         }
         return asciiArray;
+    }
+
+    public static void main(String[] args) {
+        String textInput = "Hello"; 
+        int[] asciiValues = toAscii(textInput);
+        for (int ascii : asciiValues) {
+            System.out.print(ascii + " ");
+        }
+        System.out.println();
     }
 }
