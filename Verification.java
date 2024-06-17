c8b97a09815edc061797237385c77b665013e169
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-17 10:42:05 +0200

    [change] Added a switch to convert the input

--- Verification.java
+++ Verification.java
@@ -23,6 +23,41 @@ public class Verification {
         }
         System.out.println("ASCII values of each character in " + n + ": " + Arrays.toString(asciiArray));
 
+        System.out.println("Choose your conversion between hexadecimal, binary, octal and decimal: ");
+        String choice = sc.nextLine();
+        switch (choice) {
+            case "hexadecimal":
+                StringBuilder hexValue = new StringBuilder();
+                for (int i = 0; i < n.length(); i++) {
+                    hexValue.append(Integer.toHexString(asciiArray[i]));
+                }
+                System.out.println("The hexadecimal value of " + n + " is: " + hexValue.toString());
+                break;
+            case "binary":
+                StringBuilder binaryValue = new StringBuilder();
+                for (int i = 0; i < n.length(); i++) {
+                    binaryValue.append(Integer.toBinaryString(asciiArray[i]));
+                }
+                System.out.println("The binary value of " + n + " is: " + binaryValue.toString());
+                break;
+            case "octal":
+                StringBuilder octalValue = new StringBuilder();
+                for (int i = 0; i < n.length(); i++) {
+                    octalValue.append(Integer.toOctalString(asciiArray[i]));
+                }
+                System.out.println("The octal value of " + n + " is: " + octalValue.toString());
+                break;
+            case "decimal":
+                StringBuilder decimalValue = new StringBuilder();
+                for (int i = 0; i < n.length(); i++) {
+                    decimalValue.append(asciiArray[i]);
+                }
+                System.out.println("The decimal value of " + n + " is: " + decimalValue.toString());
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
         sc.close();
     }
 }
