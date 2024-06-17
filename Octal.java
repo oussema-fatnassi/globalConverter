d4620bad8157a1fc7ef624defadd52517554a28a
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-17 11:18:36 +0200

    [change] toAscii function written using charAt

--- Octal.java
+++ Octal.java
@@ -1,5 +1,10 @@
 public class Octal {
     public static int[] toAscii (String input) {
-        return new int[0];
+        int[] asciiArray = new int[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            asciiArray[i] = (int) c;
+        }
+        return asciiArray;
     }
 }
