449a8d779402e8e2f00bc03cbcaef3c3f4382b9d
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-17 11:13:03 +0200

    [creation] Ascii class to convert from Ascii to everything

---
+++ Ascii.java
@@ -0,0 +1,21 @@
+public class Ascii {
+    public static String toDecimal (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toHexadecimal(int[] asciiArray) {
+        return "";
+    }
+
+    public static String toOctal (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toBinary (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toText (int[] asciiArray) {
+        return "";
+    }
+}
