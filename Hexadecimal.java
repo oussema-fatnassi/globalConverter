3c1c05586b2605bedab2603765c18aac3d57f06e
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-17 11:01:21 +0200

    [create] Hexadecimal class created

---
+++ Hexadecimal.java
@@ -0,0 +1,25 @@
+public class Hexadecimal {
+    public static void main(String[] args) {
+        
+    }
+
+    public static String toHexadecimal(int[] asciiArray) {
+        return "";
+    }
+
+    public static String toOctal (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toBinary (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toDecimal (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toText (int[] asciiArray) {
+        return "";
+    }
+}
