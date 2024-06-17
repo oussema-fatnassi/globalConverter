a7b619aca182b91bb54697e8f85392d9f4496f39
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-17 11:02:21 +0200

    [create] Text class created

---
+++ Text.java
@@ -0,0 +1,25 @@
+public class Text {
+    public static void main(String[] args) {
+        
+    }
+
+    public static String toText (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toHexadecimal(int[] asciiArray) {
+        return "";
+    }
+
+    public static String toOctal (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toBinary (int[] asciiArray) {
+        return "";
+    }
+
+    public static String toDecimal (int[] asciiArray) {
+        return "";
+    }
+}
