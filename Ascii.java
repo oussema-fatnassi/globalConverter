b768b609c117a99161f869d683151c2a3ea8308f
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-06-17 13:55:51 +0200

    [change] Correction of methods, not using java functions

--- Ascii.java
+++ Ascii.java
@@ -1,4 +1,5 @@
 public class Ascii {
+
     public static String toDecimal(int[] asciiArray) {
         StringBuilder decimalString = new StringBuilder();
         for (int ascii : asciiArray) {
@@ -10,7 +11,7 @@ public class Ascii {
     public static String toHexadecimal(int[] asciiArray) {
         StringBuilder hexString = new StringBuilder();
         for (int ascii : asciiArray) {
-            hexString.append(Integer.toHexString(ascii)).append(" ");
+            hexString.append(convertToHexadecimal(ascii)).append(" ");
         }
         return hexString.toString().trim();
     }
@@ -18,7 +19,7 @@ public class Ascii {
     public static String toOctal(int[] asciiArray) {
         StringBuilder octalString = new StringBuilder();
         for (int ascii : asciiArray) {
-            octalString.append(Integer.toOctalString(ascii)).append(" ");
+            octalString.append(convertToOctal(ascii)).append(" ");
         }
         return octalString.toString().trim();
     }
@@ -26,7 +27,7 @@ public class Ascii {
     public static String toBinary(int[] asciiArray) {
         StringBuilder binaryString = new StringBuilder();
         for (int ascii : asciiArray) {
-            binaryString.append(Integer.toBinaryString(ascii)).append(" ");
+            binaryString.append(convertToBinary(ascii)).append(" ");
         }
         return binaryString.toString().trim();
     }
@@ -37,6 +38,52 @@ public class Ascii {
             textString.append((char) ascii);
         }
         return textString.toString();
+    }
+
+    private static String convertToBinary(int decimal) {
+        StringBuilder binary = new StringBuilder();
+        if (decimal == 0) {
+            binary.append(0);
+        } else {
+            while (decimal != 0) {
+                int remainder = decimal % 2;
+                binary.insert(0, remainder);
+                decimal = decimal / 2;
+            }
+        }
+        return binary.toString();
+    }
+
+    private static String convertToHexadecimal(int decimal) {
+        StringBuilder hexadecimal = new StringBuilder();
+        if (decimal == 0) {
+            hexadecimal.append(0);
+        } else {
+            while (decimal != 0) {
+                int remainder = decimal % 16;
+                if (remainder < 10) {
+                    hexadecimal.insert(0, remainder);
+                } else {
+                    hexadecimal.insert(0, (char) ('A' + remainder - 10));
+                }
+                decimal = decimal / 16;
+            }
+        }
+        return hexadecimal.toString();
+    }
+
+    private static String convertToOctal(int decimal) {
+        StringBuilder octal = new StringBuilder();
+        if (decimal == 0) {
+            octal.append(0);
+        } else {
+            while (decimal != 0) {
+                int remainder = decimal % 8;
+                octal.insert(0, remainder);
+                decimal = decimal / 8;
+            }
+        }
+        return octal.toString();
     }
 
     public static void main(String[] args) {
